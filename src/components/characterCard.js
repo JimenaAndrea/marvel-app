4a2954e525059a22892f41ab0aa688392b63faf7
Jimena <jimenaa971@gmail.com>
2022-05-30 00:00:20 +0200

    Add home screen grid

--- src/components/characterCard.js
+++ src/components/characterCard.js
@@ -11,25 +11,23 @@ const CharacterCard = ({ image, name }) => {
   const [ color, setColor ] = React.useState('white')
 
   return(
-    <View style={{width: 115, height: 115}}>
-      <ImageBackground source={image} style={{flex: 1}}>
-        <LinearGradient 
-          colors={['rgba(0, 0, 0, 0.7)', 'rgba(0, 0, 0, 0)']}
-          style={{position: 'absolute', left: 0, right: 0, top: 0, height: '36.5%'}} />
-        <Pressable 
-          style={{position: 'absolute', top: 7, left: 0, right: 7, bottom: 0, justifyContent: 'flex-start', alignItems: 'flex-end'}}
-          onPress = {() => setColor(color=='#FFFFFF'? '#C72828' : '#FFFFFF')}>
-          <SimpleLineIcons color={color} name={"heart"} size={16} />
-        </Pressable>
-        <LinearGradient 
-          colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 1)']}
-          locations={[0.20, 0.94]}
-          style={{position: 'absolute', left: 0, right: 0, bottom: 0, height: '36.5%'}} />
-        <View style={{position: 'absolute', top: 0, left: 8, right: 0, bottom: 11, justifyContent: 'flex-end', alignItems: 'flex-start'}}>
-          <Text style={{color: 'white', fontSize: 12, fontFamily: 'Inter_600SemiBold', fontWeight: '600', fontStyle: 'normal'}}>{name}</Text>
-        </View>
-      </ImageBackground>
-    </View>
+    <ImageBackground source={image} style={{ aspectRatio: 1 }}>
+      <LinearGradient 
+        colors={['rgba(0, 0, 0, 0.7)', 'rgba(0, 0, 0, 0)']}
+        style={{position: 'absolute', left: 0, right: 0, top: 0, height: '36.5%'}} />
+      <Pressable 
+        style={{position: 'absolute', top: 7, left: 0, right: 7, bottom: 0, justifyContent: 'flex-start', alignItems: 'flex-end'}}
+        onPress = {() => setColor(color=='#FFFFFF'? '#C72828' : '#FFFFFF')}>
+        <SimpleLineIcons color={color} name={"heart"} size={16} />
+      </Pressable>
+      <LinearGradient 
+        colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 1)']}
+        locations={[0.20, 0.94]}
+        style={{position: 'absolute', left: 0, right: 0, bottom: 0, height: '36.5%'}} />
+      <View style={{position: 'absolute', top: 0, left: 8, right: 0, bottom: 11, justifyContent: 'flex-end', alignItems: 'flex-start'}}>
+        <Text style={{color: 'white', fontSize: 12, fontFamily: 'Inter_600SemiBold', fontWeight: '600', fontStyle: 'normal'}}>{name}</Text>
+      </View>
+    </ImageBackground>
   )
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,111 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { CharacterCard } from '../components';
+import { FlatGrid } from 'react-native-super-grid';
 
-const HomeScreen = ({ navigation }) => (
-    <View>
-        <Text>Hello World!</Text>
-        <Button title='Press me' onPress={() => navigation.navigate('Character')} />
-    </View>
-)
+function renderItem({ item }) {
+  return(
+    <CharacterCard image={item.image} name={item.name} />
+  )
+}
+
+const HomeScreen = () => {
+  return(
+    <FlatGrid
+      itemDimension={100}
+      data={data}
+      renderItem={renderItem}
+      maxItemsPerRow={3}
+      spacing={10}
+    />
+  )
+}
+
+const data = [
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+  {
+    name: 'Avengers',
+    image: require('../../assets/3dman442.jpg')
+  },
+
+]
 
 export default HomeScreen;
