52e7da7e8c0c2786dbe9cbd6412afaab0a5b1973
Jimena <jimenaa971@gmail.com>
2022-05-30 20:38:14 +0200

    Implement FavouritesScreen funcionality
    - Filter Marvel API characters to show only favorites.
    - Display characters in a 2-column grid.

--- src/screens/FavouritesScreen.js
+++ src/screens/FavouritesScreen.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import { CharacterCard } from '../components';
+import { FlatGrid } from 'react-native-super-grid';
+import { requestCharacters } from '../marvelAPI';
+import { useQuery } from 'react-query';
 
-const FavouritesScreen = ({ navigation }) => (
-    <View>
-        <Text>Hello World!</Text>
-        <Button title='Press me' onPress={() => navigation.navigate('Character')} />
-    </View>
-)
+function renderItem({ item }) {
+  const image = {uri: item.thumbnail.path + '/standard_xlarge.' + item.thumbnail.extension};
+  
+  return(
+    <CharacterCard image={image} name={item.name} id={item.id} />
+  )
+}
+
+const FavouritesScreen = () => {
+  const { data, status } = useQuery('requestCharacters', requestCharacters);
+  const favState = useSelector(state => state.favourites);
+  const favourites = data.filter(element => favState[element.id]);
+
+  return(
+    status==='loading' ?
+      <Text>Loading</Text> 
+      :
+      <FlatGrid
+        itemDimension={100}
+        data={favourites}
+        renderItem={renderItem}
+        maxItemsPerRow={2}
+        spacing={16}
+      />
+  )
+}    
 
 export default FavouritesScreen;
